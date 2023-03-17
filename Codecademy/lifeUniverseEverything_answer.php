9fa19a634469c7f493cbc3632e8d3f095a5a24d9
Lopezcartoons <lopezcartoons@gmail.com>
2023-03-16 22:58:56 -0500

    php test

---
+++ Codecademy/lifeUniverseEverything_answer.php
@@ -0,0 +1,16 @@
+<?php
+function lifeUniverseEverything(){
+$initialize = '555';
+$a = octdec($initialize);
+$b = deg2rad($a);
+$c = cos($b);
+$d = round($c, 3);
+$e = log($d); 
+$f = abs($e);
+$g = acos($f);
+$h = rad2deg($g);
+$i = floor($h);
+$j = $i - 47;
+echo $j . "\n";}
+
+lifeUniverseEverything();
